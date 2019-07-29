9ce0619349807a4855357185d5ceefb37627e704
Yura Lee <0x1f440@gmail.com>
2019-07-30 01:07:50 +0900

    세미나 자료 추가

---
+++ Assets/RotateCharacter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCharacter : MonoBehaviour {	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<RectTransform>().Rotate(new Vector3(0, 3, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/ChangeClothes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeClothes : MonoBehaviour {
+
+	public void ChangeSprite(){
+		GameObject.FindGameObjectWithTag("Player").GetComponent<Image>().sprite = GetComponent<Image>().sprite;
+		GetComponent<AudioSource>().Play();
+	}
+}
